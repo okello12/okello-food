e7aab82057e1de25ef159c69003da3788846cbda
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-06 12:40:24 +0100

    Update in-app refresh controls for v14

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v12';
+  const APP_VERSION = 'v14';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
