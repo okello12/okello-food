db3cbfa2bb44b79a3e1e1e9cf414989a00565a21
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-06 13:25:52 +0100

    Show v17 in app update controls

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v16';
+  const APP_VERSION = 'v17';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
