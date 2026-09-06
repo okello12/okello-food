c01db4251a583fce8e3cf891522d06bda3619db0
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-06 18:15:14 +0100

    Report v30 app bundle

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v29';
+  const APP_VERSION = 'v30';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
