3cf734905547d4cefa7f0f59d52561167034274a
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-06 13:48:16 +0100

    Bump app updater to v19

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v18';
+  const APP_VERSION = 'v19';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
