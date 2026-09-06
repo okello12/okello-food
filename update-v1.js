01e84c06dfbc56579080010f4e71b9c7f88aa718
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-06 13:32:45 +0100

    Mark updater as v18

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v17';
+  const APP_VERSION = 'v18';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
