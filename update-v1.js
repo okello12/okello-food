609ac428f3383952d64e5659ee2346963bf57fab
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-07 18:42:11 +0100

    Advance update indicator to v41

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v40';
+  const APP_VERSION = 'v41';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
