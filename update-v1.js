01ad652a651e0bce6f4766eabbd16c581d34c0ee
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-07 21:02:13 +0100

    Show v42 update bundle

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v41';
+  const APP_VERSION = 'v42';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
