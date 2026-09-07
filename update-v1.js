e2263618bdec690086ecf5bd9fc6ae1966a9f971
okello12 <56190257+okello12@users.noreply.github.com>
2026-09-07 01:13:33 +0100

    Report app bundle v38

--- update-v1.js
+++ update-v1.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const APP_VERSION = 'v37';
+  const APP_VERSION = 'v38';
   const UPDATE_KEY = 'okello_last_update_check';
   const RELOAD_KEY = 'okello_controller_reload';
 
